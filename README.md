abd1b84d7d0d10eeb358d1441d7822ee27bf16a2
snlw <seanlow22@gmail.com>
2022-04-05 16:20:13 +0800

    understanding the react lifecycle

--- README.md
+++ README.md
@@ -34,6 +34,41 @@
 ## Tips
 ### React
 1. Relative imports outside of src/ are not supported.
+2. Class names of div (with styles specified in css) should not contain '-' as concatenation. It should use " ", else the div will not render.
+
+### React Lifecycle
+Overview of the component lifecycle
+1. The initial mount lifecycle
+  - This is when React attempts to build the component before pushing it into the DOM.
+  - The mount lifecycle is called twice: before and immediately after React renders the component into the DOM.
+  - The `componentDidMount()` lifecycle method is better suited for functions that take an indeterminate amount of time to load. `componentDidMount()` is called after `render()` is called, so you don't get any weird behaviour where the component takes a long time before it actually shows up on the page.
+  - One common best practice is to render an elemnt that informs the reader that the page is loading, rather than having the initializing state to be empty.
+  ```js
+  constructor()
+  static getDerivedStateFromProps()
+  render()
+  componentDidMount()
+  ```
+
+2. The update lifecycle
+  - This is when the component has been loaded and is in a state of watching for updates
+  - `componentDidUpdate(prevProps, prevState, snapshot)`
+    - `prevProps` is the shape of the component's props before the update happened.
+    - `prevState` is the shape of the component's state before the update happened.
+    - `snapshot` is the virtual representation of the component before the update.
+  ```js
+  static getDerivedStateFromProps()
+  shouldComponentUpdate()
+  render()
+  getSnapshotBeforeUpdate()
+  componentDidUpdate()
+  ```
+
+3. The unmount lifecycle
+  - This only comes into play when a component is removed from the DOM.
+  ```js
+  componentWillUnmount()
+  ```
 
 ### For Windows Users who prefer to use WSL2.
 - Purpose : To enable Hot Reload (while your application is running, you can make changes to the code and apply them to the running application [Link](https://blog.jetbrains.com/dotnet/2021/12/02/how-rider-hot-reload-works-under-the-hood/#:~:text=let's%20dive%20in!-,What%20does%20Hot%20Reload%20do%3F,your%20application%20is%20kept%20intact.))
--- the-react-workshop/src/apps/App.js
+++ the-react-workshop/src/apps/App.js
@@ -58,8 +58,114 @@ class BuyStuffApp extends Component {
 
 };
 
+class ConditionalRenderingApp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showSecret  : false
+    };
+    this.toggleSecretMessage = this.toggleSecretMessage.bind(this);
+  };
+  
+  displaySecretMessage() {
+    // Guard clause is a simple statement at the top of the function that verifies input, properties
+    // or state and returns early to avoid unnecessary work.
+    if (!this.state.showSecret){
+      return;
+    }
+    return (
+      <p>There is no secret.</p>
+    );
+  };
+  
+  toggleSecretMessage(event) {
+    const showSecret = !this.state.showSecret;
+    this.setState({showSecret});
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          style = {{
+            fontWeight : "bold",
+            height : "10em"
+          }}
+          onClick = {this.toggleSecretMessage}
+          >Click to show the secret message!</button>
+          {this.state.showSecret ? this.displaySecretMessage() : ""}
+      </div>
+    );
+  }
+}
+
+class UnderstandingLifecycleApp extends Component {
+  // First part of the mount lifecycle.
+  constructor(props) {
+    super(props);
+    console.log('Constructor');
+    this.state = {
+      messages : [],
+      loading : true
+    };
+  };
+
+  renderProfile() {
+    if (this.state.loading){
+      return (<div>
+        Loading...
+      </div>)
+    }
+    if (this.state.messages.length > 0){
+      return (
+        <ul>
+          {this.state.messages.map((msg, idx)=> <li key={`msg-${idx}`}>{msg}</li>)}
+        </ul>
+      )
+    }
+    else {
+      return (
+        <ul>
+          No Messages
+        </ul>
+      );
+    };
+  };
+
+  // Last part of the mount lifecycle
+  componentDidMount() {
+    console.log('Component Did Mount');
+    setTimeout(() => this.setState({
+      messages : ['hi', 'yo'],
+      loading : false
+    }), 10000);
+  };
+
+  // Last part of the update lifecycle
+  componentDidUpdate(prevProps, prevState) {
+    console.log(`prevProps : ${prevProps}`);
+    console.log(`prevState : ${prevState}`);
+  };
+
+  render() {
+    // Second part of the mount lifecycle.
+    console.log('Render')
+    return (
+      <div>
+        <h1>User Profile</h1>
+        <hr />
+        {this.renderProfile()}
+        
+      </div>
+    )
+  }
+}
+
 export {
   FunctionalApp,
   ClassApp,
-  BuyStuffApp
+  BuyStuffApp,
+  ConditionalRenderingApp,
+  UnderstandingLifecycleApp
 };
