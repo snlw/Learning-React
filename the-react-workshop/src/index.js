3779cdf48b06c73e81bc15c362c845a859a346b9
snlw <seanlow22@gmail.com>
2022-04-06 17:50:13 +0800

    final commit

--- the-react-workshop/src/index.js
+++ the-react-workshop/src/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 // ReactDOM is a library that provides interaction with the DOM layer of the browser.
 import ReactDOM from 'react-dom';
 import './index.css';
-import {
-  // FunctionalApp, 
-  // ClassApp,
-  // BuyStuffApp,
-  ConditionalRenderingApp} from './apps/App';
+// import {UnderstandingLifecycleApp} from './apps/App';
 // import LoginApp from './apps/LoginApp';
 // import BlogApp from './apps/BlogApp';
+// import QuizApp from './apps/QuizApp';
+// import App from './apps/ProfileApp';
+// import App from './apps/CommunicationBetweenComponents';
+// import MemoryGameApp from './apps/MemoryGameApp';
+// import TemperatureConverter from './apps/TemperatureConverterApp';
+// import ContextApp from './apps/ContextApp'
+// import Counter from './apps/useReducer.example';
+// import NameFields from './apps/useId.example';
+import App from './apps/useState.example';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   // StrictMode helps to spot potential errors in advance.
   // It performs code checks only during development time and it does not work in the production run.
   <React.StrictMode>
-    <ConditionalRenderingApp />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
